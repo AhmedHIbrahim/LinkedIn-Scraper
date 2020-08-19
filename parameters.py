1177e2e8e1ea3402bfe483d7b007f61a1552343a
AhmedHIbrahim <ah1.6.1998@gmail.com>
2020-08-19 20:15:08 +0300

    Linkedin Selenium Scraper

---
+++ parameters.py
@@ -0,0 +1,17 @@
+# Location of Chrome driver in your local device
+chromeWebDriverPath = 'D:\chromedriver'
+
+
+linkedIn_login_page = 'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin'
+
+googleUrl = 'https://www.google.com'
+
+# login details
+in_email = 'exampleEmail@gmail.com'
+in_password = 'examplePassword'
+
+# Search details
+google_search_query = 'site:linkedin.com/in/ AND "Software Engineer" AND "Berlin"'
+
+# CSV file name
+csv_file_name='LinkedIn_Profiles_Info.csv'
